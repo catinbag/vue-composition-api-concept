4930dd0bb09c932244fb662f72ce06efa624ac96
Silin Dmitry <true.litry@gmail.com>
2022-12-04 15:32:14 +0300

    init

---
+++ src/components/TechCrunch.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <label
+      >Category:
+      <select v-model="filters.categories">
+        <option v-for="category in availableCategories" :value="category.id">{{ category.name }}</option>
+      </select>
+    </label>
+    <hr />
+    <div>
+      <button :disabled="page === 1" @click="page -= 1">Prev</button>
+      {{ page }}
+      <button @click="page += 1">Next</button>
+    </div>
+    <hr />
+
+    <ul>
+      <li v-for="item in items">
+        <a target="_blank" :href="item.link" v-html="item.title.rendered"></a>
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+import { getPosts, getCategories } from "../api/techcrunch";
+import { filterableMixin } from "./mixins/filterable";
+
+export default {
+  mixins: [filterableMixin],
+  data() {
+    return {
+      categories: [],
+    };
+  },
+  computed: {
+    availableCategories() {
+      return [{ id: null, name: "(no category)" }, ...this.categories];
+    },
+  },
+  methods: {
+    async loadCategories() {
+      this.categories = await getCategories();
+    },
+
+    async loadItems() {
+      this.items = await getPosts({
+        page: this.page,
+        filters: this.filters,
+      });
+    },
+  },
+
+  created() {
+    this.loadCategories();
+  },
+};
+</script>
+1
