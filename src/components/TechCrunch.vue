20dd874ac7085275a52130b87583199310fee2e5
Silin Dmitry <true.litry@gmail.com>
2022-12-04 15:51:59 +0300

    update deps

--- src/components/TechCrunch.vue
+++ src/components/TechCrunch.vue
@@ -3,7 +3,9 @@
     <label
       >Category:
       <select v-model="filters.categories">
-        <option v-for="category in availableCategories" :value="category.id">{{ category.name }}</option>
+        <option v-for="category in availableCategories" :key="category.id" :value="category.id">
+          {{ category.name }}
+        </option>
       </select>
     </label>
     <hr />
@@ -15,44 +17,43 @@
     <hr />
 
     <ul>
-      <li v-for="item in items">
-        <a target="_blank" :href="item.link" v-html="item.title.rendered"></a>
+      <li v-for="item in items" :key="item.id">
+        <a target="_blank" :href="item.link" v-html="item.title.rendered" />
       </li>
     </ul>
   </div>
 </template>
 <script>
-import { getPosts, getCategories } from "../api/techcrunch";
-import { filterableMixin } from "./mixins/filterable";
+import { getPosts, getCategories } from '../api/techcrunch'
+import { filterableMixin } from './mixins/filterable'
 
 export default {
   mixins: [filterableMixin],
   data() {
     return {
-      categories: [],
-    };
+      categories: []
+    }
   },
   computed: {
     availableCategories() {
-      return [{ id: null, name: "(no category)" }, ...this.categories];
-    },
+      return [{ id: null, name: '(no category)' }, ...this.categories]
+    }
   },
   methods: {
     async loadCategories() {
-      this.categories = await getCategories();
+      this.categories = await getCategories()
     },
 
     async loadItems() {
       this.items = await getPosts({
         page: this.page,
-        filters: this.filters,
-      });
-    },
+        filters: this.filters
+      })
+    }
   },
 
   created() {
-    this.loadCategories();
-  },
-};
+    this.loadCategories()
+  }
+}
 </script>
-1
