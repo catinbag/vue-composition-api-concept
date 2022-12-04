aa44a6354c5e1b1ff13335a6c7f651e9cc2982e0
Dmitry <43723172+catinbag@users.noreply.github.com>
2022-12-05 01:03:15 +0300

    refactor: update components (#2)

--- src/components/TechCrunch.vue
+++ src/components/TechCrunch.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <label
-      >Category:
+    <label>
+      Category:
       <select v-model="filters.categories">
         <option v-for="category in availableCategories" :key="category.id" :value="category.id">
           {{ category.name }}
@@ -9,25 +9,26 @@
       </select>
     </label>
     <hr />
-    <div>
-      <button :disabled="page.value === 1" @click="prevPage">Prev</button>
-      {{ page.value }}
-      <button @click="nextPage">Next</button>
-    </div>
+    <ThePagination :page="page" @next-page="nextPage" @prev-page="prevPage" />
     <hr />
-
     <ul>
       <li v-for="item in items.value" :key="item.id">
-        <a target="_blank" :href="item.link" v-html="item.title.rendered" />
+        <a target="_blank" :href="item.link">{{ item.title.rendered }}</a>
       </li>
     </ul>
   </div>
 </template>
+
 <script>
-import { getPosts, getCategories } from '../api/techcrunch'
+import ThePagination from './Pagination.vue'
 import { useFilterable } from './composables/filterable'
+import { getPosts, getCategories } from '../api/techcrunch'
 
 export default {
+  name: 'TechCrunchView',
+  components: {
+    ThePagination
+  },
   data() {
     return {
       categories: []
@@ -44,15 +45,10 @@ export default {
     }
   },
   setup() {
-    const { page, nextPage, prevPage, filters, items } = useFilterable(
-      {
-        loadItems: getPosts,
-        initFilter: { categories: null }
-      },
-      this
-    )
-
-    this.loadCategories()
+    const { page, nextPage, prevPage, filters, items } = useFilterable({
+      loadItems: getPosts,
+      initFilter: { categories: null }
+    })
 
     return {
       page,
@@ -61,6 +57,9 @@ export default {
       filters,
       items
     }
+  },
+  created() {
+    this.loadCategories()
   }
 }
 </script>
